6ccecee748514a3e1d0f5f0bb99f9c4a430c9c1a
David Hopkins <dlhopkins2@gmail.com>
2018-07-04 00:28:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-let currentUser = "John Doe";
+let currentUser = "Grace Hopper";
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
 let shortGreeting = "Welcome, " + currentUser + "!"
